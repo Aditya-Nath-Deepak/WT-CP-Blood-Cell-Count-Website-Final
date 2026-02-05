b710bd10b7eff9a30843d750d5a969b2d88ee5a7
Aditya Nath Deepak <adityanathdeepak2005@gmail.com>
2026-02-06 04:35:49 +0530

    Final setup: Fixed dependencies and enabled mobile tunneling

--- blood_cell_detection/settings.py
+++ blood_cell_detection/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '*7nl#k0ug*78t@rnfg0!02nw@($-(_783x47hgiw-#4jn#)ren'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
